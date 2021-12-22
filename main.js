965625c2531288ffa9d65077a997e63193fc6658
Anis4whitehat <82126047+Anis4whitehat@users.noreply.github.com>
2021-12-21 20:55:23 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function setup(){
     canvas = createCanvas(280, 280);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
 }
 
 function updateCanvas(){
@@ -28,5 +29,28 @@ function draw(){
     if (mouseIsPressed) { line(pmouseX, pmouseY, mouseX, mouseY);}
     check_sketch()
     if(drawn_sketch == sketch)
-    { answer_holder = "set" score++; document.getElementById('score').innerHTML = 'Score: ' + score; }
+    { answer_holder = "set"
+    score++; document.getElementById('score').innerHTML = 'Score: ' + score; }
 }
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results)
+ { if (error) { console.error(error); } 
+ console.log(results);
+  drawn_sketch = results[0].label;
+document.getElementById('label').innerHTML = 'Your Sketch: ' + drawn_sketch; document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%'; }
+
+function check_sketch(){
+    timer_counter++;
+    document.getElementById('time').innerHTML = 'timer'+timer_counter;
+    if(timer_counter>400)
+        { timer_counter = 0; timer_check = "completed" }
+         if(timer_check =="completed" || answer_holder == "set")
+         { timer_check = ""; answer_holder = ""; updateCanvas(); } }
